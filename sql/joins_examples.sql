77d43454d56859c0cb74ec686744a26952b83d94
wilbon <wilbonkiprono25@gmail.com>
2026-03-04 15:25:46 +0300

    Update joins_examples.sql

--- sql/joins_examples.sql
+++ sql/joins_examples.sql
@@ -1,1 +1,17 @@
+-- INNER JOIN Example
+SELECT
+    e.name,
+    d.department_name
+FROM employees e
+INNER JOIN departments d
+ON e.department_id = d.department_id;
 
+
+
+-- Window Function Example
+
+SELECT
+    name,
+    salary,
+    ROW_NUMBER() OVER (ORDER BY salary DESC) AS salary_rank
+FROM employees;
